33552c258dababf3bb2e6e11e0b43733ba28ab4e
AnnaMikshina <wow154@mail.ru>
2023-01-27 22:37:11 +0500

    Задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int Prompt(string message)
+{
+    Console.WriteLine(message + ">");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int x1 = Prompt("Введите координаты точки А по оси x");
+int y1 = Prompt("Введите координаты точки А по оси y");
+int z1 = Prompt("Введите координаты точки А по оси z");
+int x2 = Prompt("Введите координаты точки В по оси x");
+int y2 = Prompt("Введите координаты точки В по оси y");
+int z2 = Prompt("Введите координаты точки В по оси z");
+
+
+double result  = Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+
+
+Console.WriteLine($"Длина отрезка равна {result}");
