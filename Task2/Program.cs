559414f3960b03f545b84185c6aeac41cf8a52f9
AnnaMikshina <wow154@mail.ru>
2023-01-27 22:57:30 +0500

    Задача 3

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -15,9 +15,9 @@ int y2 = Prompt("Введите координаты точки В по оси y
 int z2 = Prompt("Введите координаты точки В по оси z");
 
 
-double result  = Math.Sqrt(Math.Pow((x2-x1), 2) + 
-                   Math.Pow((y2-y1), 2) + 
-                   Math.Pow((z2-z1), 2));
+double result = Math.Sqrt(Math.Pow((x2 - x1), 2) +
+                   Math.Pow((y2 - y1), 2) +
+                   Math.Pow((z2 - z1), 2));
 
 
 Console.WriteLine($"Длина отрезка равна {result}");
